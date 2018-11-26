02cb28829d366bf79b3529cc6b584f0c50180930
Nicole Mazzuca <npmazzuca@gmail.com>
2018-11-25 18:05:59 -0800

    move things around

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,39 @@
-pub mod stable_map;
+#![cfg_attr(feature = "nightly", feature(marker_trait_attr))]
+
+pub mod stable_set;
+//pub mod stable_map;
+
+use std::{ops::Deref, borrow::Borrow};
+
+mod impls;
+
+pub struct Atom<'a, T: ?Sized> {
+  __ptr: &'a T,
+}
+
+impl<'a, T: ?Sized> Atom<'a, T> {
+  fn new(__ptr: &'a T) -> Self {
+    Atom { __ptr }
+  }
+
+  pub fn as_ref(p: Self) -> &'a T {
+    p.__ptr
+  }
+}
+
+/**
+  This trait guarantees that, for a type T: StablePointer,
+  given a value `t: &'a T`:
+
+    `&**t` shall live for at least `'a`
+
+  i.e., when `*t` moves, the underlying `<T as Deref>::Target` does not move
+*/
+#[cfg_attr(feature = "nightly", marker)]
+pub unsafe trait StablePointer: Deref + Borrow<<Self as Deref>::Target> {}
+
+unsafe impl<T: ?Sized> StablePointer for Box<T> {}
+unsafe impl<T: ?Sized> StablePointer for std::rc::Rc<T> {}
+unsafe impl<T: ?Sized> StablePointer for std::sync::Arc<T> {}
+unsafe impl<T> StablePointer for Vec<T> {}
+unsafe impl StablePointer for String {}
---
+++ src/stable_set.rs
@@ -0,0 +1,51 @@
+use std::{cell, collections};
+use std::{cmp::Ord, ops::Deref, borrow::Borrow};
+use std::marker::PhantomData;
+
+use super::{Atom, StablePointer};
+
+pub struct StableSet<T: StablePointer, CmpObject = <T as Deref>::Target> {
+  // for safety, this must be append-only
+  // _never_ remove any values from it
+  set: cell::UnsafeCell<collections::BTreeSet<T>>,
+  marker: PhantomData<CmpObject>,
+}
+
+impl<T, CmpObject> StableSet<T, CmpObject>
+where
+  T: StablePointer + Ord + Borrow<CmpObject>,
+  CmpObject: Ord,
+{
+  pub fn new() -> Self {
+    StableSet {
+      set: cell::UnsafeCell::new(collections::BTreeSet::new()),
+      marker: PhantomData,
+    }
+  }
+
+  pub fn add_element<'a, 'b, U>(
+    &'a self, element: &'b U,
+  ) -> Atom<'a, <T as std::ops::Deref>::Target>
+    where
+      U: Borrow<CmpObject>,
+      &'b U: Into<T>,
+  {
+    unsafe {
+      // safe because we don't allow anybody to get a reference to the innards
+      // without an indirection
+      // and because we never remove
+      let name_cmp = element.borrow();
+      let inner = &mut *self.set.get();
+      if let Some(b) = inner.get(name_cmp) {
+        let buf = &*(b as *const T);
+        Atom::new(buf.borrow())
+      } else {
+        inner.insert(element.into());
+        // this seems unnecessary, but BTreeSet doesn't have a full interface
+        // also, break the lifetime relation between inner and the ref
+        let buf = &*(inner.get(name_cmp).unwrap() as *const T);
+        Atom::new(buf.borrow())
+      }
+    }
+  }
+}
